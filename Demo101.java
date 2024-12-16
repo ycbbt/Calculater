3490eb6045444c926b4fa860f7f3a93bcb38286e
ycbbt <1709262270@qq.com>
2024-12-16 13:52:53 -0800

    Add files via upload

---
+++ Demo101.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Demo101 {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter your first number");
+        double num1=sc.nextDouble();
+        System.out.println("please enter you(+, —, *, /):");
+        String op=sc.next();
+        System.out.println("Enter your second number");
+        double num2=sc.nextDouble();
+        double result=calc(num1,op,num2);
+        System.out.println(num2+" "+op+" "+num2+" = "+result);
+    }
+ 
+    public static double calc(double num1, String op, double num2) {
+        double result=0;
+        switch (op){
+            case "+":
+                result=num1+num2;
+                break;
+            case "-":
+                result=num1-num2;
+                break;
+            case "*":
+                result=num1*num2;
+                break;
+            case "/":
+                result=num1/num2;
+                break;
+            default:
+                System.out.println("请输入有效运算符");
+        }
+        return result;
+    }
+}
